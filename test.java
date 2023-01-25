a5193102a21426ac1834c5c4c90e9bdd7030e40d
LANKAI MILLS flavien <evansflavien@gmail.com>
2023-01-25 13:58:15 +0100

    test

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
-public class test{
+public class test {
     public static void main(String[] args) {
-        System.out.println("Test tEst teSt tesT");
+        System.out.println("Test test test test");
     }
 }
