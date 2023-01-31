62bddcd200bf5564f39006fd664c89f05958dda5
LANKAI MILLS flavien <evansflavien@gmail.com>
2023-01-31 19:09:18 +0100

    test page multiple

--- src/model/balle.java
+++ src/model/balle.java
@@ -2,25 +2,28 @@ package model;
 
 import javax.swing.JPanel;
 
+import view.Visuel;
+
 import java.awt.Color;
 import java.awt.Graphics;
 public class balle extends JPanel{//la balle du joueur
     //attributs
-    int x,y;//ses coordonnées
+    public int x,y;//ses coordonnées
     double speedX,speedY;//sa vitesse
     // TODO : ajouter les autres attributs
 
     public void deplacement(){
-        this.x += 10;// (int)speedX;
-        this.y = (int)speedY;
-        //TODO : collisions bordures jeu, peg...
+        if(Visuel.enJeu){
+            this.x += 10;// (int)speedX;
+            this.y = (int)speedY;
+            //TODO : collisions bordures jeu, peg...
+        }
     }
 
     @Override
     public void paint(Graphics g){
         super.paint(g);
-        g.setColor(Color.red);
-        g.fillOval(this.x,this.y,30,30);
+        g.fillOval(this.x,this.y,10,10);
     }
 }
 
--- src/view/Visuel.java
+++ src/view/Visuel.java
@@ -1,26 +1,78 @@
 package view;
 
 import model.*;
-import javax.swing.JFrame;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
 public class Visuel extends JFrame {
-    public boolean enJeu = true;//Pour mettre le jeu en pose si besoin
+    public static boolean enJeu = true;//Pour mettre le jeu en pause si besoin
     balle b = new balle();
+    balle c = new balle();
+
+    JPanel premierePage = new JPanel();
+    JPanel pageActuelle = new JPanel();//Page qui est affiché
+    JButton boutton = new JButton("Jouer");
+
     public Visuel(){
         this.setTitle("Hit the Peggles");
         this.setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.getContentPane().add(b);
+
+        JPanel pageActuelle = premierePage;
+        this.getContentPane().add(pageActuelle);
+        JPanel secondePage = b;//Page avec la balle
+        JPanel troisiemePage = new JPanel();
+        troisiemePage.setBackground(Color.BLUE);
+        troisiemePage.add(new JPanel());
+
+        secondePage.setLayout(new GridBagLayout());
+        //secondePage.add(b);
+        premierePage.add(boutton);
+        premierePage.setBackground(Color.black);
+
+        b.setBackground(Color.gray);
+        
+        boutton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                changePage(secondePage);
+            }
+        });
+        secondePage.setName("a");
+        pageActuelle.setName("a");
         while(enJeu){
-            b.deplacement();
-            b.repaint();
+            if(!secondePage.getName().equals(pageActuelle.getName())){
+                b.deplacement();
+                b.repaint();
+            }
             try {
                 Thread.sleep(30);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+            if(b.x >= 800){
+                b.x = 0;
+                changePage(troisiemePage);
+            }
         }
+    }
+
+    public void changePage(JPanel nouvellePage){
+        this.getContentPane().remove(pageActuelle);
+        this.getContentPane().add(nouvellePage);
+        pageActuelle = nouvellePage;
+        nouvellePage.setName("P");
+        revalidate();
+        repaint();
+        enJeu = true;
+    }
+
+    public static void main(String[] args) {
+        new Visuel();
     }
 }
