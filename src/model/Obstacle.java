eb442c235402942101a2e1e39f8af045e00e9fb6
JIRAUD Loïc <loicjiraud@eduroam-prg-gm-1-3-37.net.univ-paris-diderot.fr>
2023-02-03 15:32:36 +0100

    maj

--- src/model/Obstacle.java
+++ src/model/Obstacle.java
@@ -3,9 +3,11 @@ package model;
 public class Obstacle extends Corps{
 
     protected boolean estMort = false;
+    protected int vie;
 
-    public Obstacle(double x, double y, double largeur, double hauteur) {
+    public Obstacle(double x, double y, double largeur, double hauteur, int vie) {
         super(x, y, largeur, hauteur);
+        this.vie = vie;
     }
     
 }
