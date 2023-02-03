59a8fa89941ee1db53f483f05ec17d6b99334884
cesa <matthew.cesa@etu.u-paris.fr>
2023-02-03 14:47:09 +0100

    update

--- src/view/Visuel.java
+++ src/view/Visuel.java
@@ -1,20 +1,25 @@
 package view;
 
-import model.*;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+// import controller.main;
 
 public class Visuel extends JFrame {
     public boolean enJeu = true;//Pour mettre le jeu en pose si besoin
-    balle b = new balle();
+    // balle b = new balle();
     public Visuel(){
         this.setTitle("Hit the Peggles");
         this.setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.getContentPane().add(b);
+        initialisationDuJeu(this);
+        // this.getContentPane().add(b);
         while(enJeu){
-            b.deplacement();
-            b.repaint();
+            // b.deplacement();
+            // b.repaint();
             try {
                 Thread.sleep(30);
             } catch (InterruptedException e) {
@@ -23,4 +28,17 @@ public class Visuel extends JFrame {
             }
         }
     }
+    public void initialisationDuJeu(JFrame frame){
+        JPanel panel = new JPanel();
+        panel.setSize(frame.getWidth(), frame.getHeight());
+        panel.setLayout(null);
+        JLabel label = new JLabel("Hit the peggles");
+        JButton btn = new JButton("HIT IT!");
+        label.setBounds(panel.getWidth()/3, panel.getHeight()/2, 100, 100);
+        btn.setBounds(label.getWidth()+label.getWidth()/2, label.getHeight()-label.getHeight()/2, 50, 50);
+        panel.add(btn);
+        panel.add(label);
+        frame.add(panel);
+    }
+
 }
