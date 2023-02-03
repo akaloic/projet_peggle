20e29f3480e96b1452da0677bf6b1253c68d0b31
JIRAUD Loïc <loicjiraud@eduroam-prg-gm-1-3-37.net.univ-paris-diderot.fr>
2023-02-03 15:04:05 +0100

    maj

--- src/model/Corps.java
+++ src/model/Corps.java
@@ -3,12 +3,16 @@ package model;
 public class Corps {
     protected double x;
     protected double y;
+    protected double largeur;
+    protected double hauteur;
     protected double vitesseX;
     protected double vitesseY;
     
-    public Corps (double x, double y, double vitesseX, double vitesseY) {
+    public Corps (double x, double y, double largeur, double hauteur, double vitesseX, double vitesseY) {
       this.x = x;
       this.y = y;
+      this.largeur = largeur;
+      this.hauteur = hauteur;
       this.vitesseX = vitesseX;
       this.vitesseY = vitesseY;
     }
@@ -16,5 +20,37 @@ public class Corps {
     public void bouger(double temps) {
       x = x + vitesseX * temps;
       y = y + vitesseY * temps;
+    }
+    
+    public double getX() {
+      return x;
+    }
+    
+    public double getY() {
+      return y;
+    }
+    
+    public double getLargeur() {
+      return largeur;
+    }
+    
+    public double getHauteur() {
+      return hauteur;
+    }
+    
+    public double getVitesseX() {
+      return vitesseX;
+    }
+    
+    public double getVitesseY() {
+      return vitesseY;
+    }
+    
+    public void setVitesseX(double vitesseX) {
+      this.vitesseX = vitesseX;
+    }
+    
+    public void setVitesseY(double vitesseY) {
+      this.vitesseY = vitesseY;
     }
 }
