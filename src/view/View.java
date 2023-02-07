a27ddb6ca28ead645ba47805c7c00bae4b2a2e97
JIRAUD Loïc <loicjiraud@MacBook-Pro-de-JIRAUD.local>
2023-02-07 14:20:32 +0100

    maj

--- src/view/View.java
+++ src/view/View.java
@@ -16,7 +16,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class View extends JFrame implements MouseInputListener{
+public class View extends JFrame implements MouseInputListener {
 
     public boolean enJeu = Visuel.enJeu;
     private JPanel balle;
@@ -26,22 +26,17 @@ public class View extends JFrame implements MouseInputListener{
     private JPanel munition;
     private JPanel partie;
     private JButton leave = new JButton("Fermer");
-<<<<<<< HEAD
     private boolean enJeu = true;
     private int angle = 0;
     private Controleur controleur;
-=======
 
     /* Variable balle */
     protected double xballSpeed = 10; // vitesse de la balle en abscisse
     protected double yballSpeed = 10; // vitesse de la balle en ordonnees
-    protected double xPos,yPos; // et position de la balle 
-    protected double max_x,max_y; //pour permettre a la balle de rebondir sur les bords
+    protected double xPos, yPos; // et position de la balle
+    protected double max_x, max_y; // pour permettre a la balle de rebondir sur les bords
 
     /* Variable Canon */
-
-    protected Controleur controleur;
->>>>>>> matthew
 
     public View(Controleur controleur) {
 
@@ -53,15 +48,7 @@ public class View extends JFrame implements MouseInputListener{
         this.setTitle("Hit the Peggles");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setUndecorated(true);
-<<<<<<< HEAD
         this.setVisible(true);// nécessaire sinon this.getHeight et this.getWidth renvoie 0
-=======
-        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = (int)size.getWidth();
-        int height = (int)size.getHeight();
-        this.setSize(width,height);
-        this.setVisible(true);//nécessaire sinon this.getHeight et this.getWidth renvoie 0
->>>>>>> matthew
 
         this.controleur = controleur;
         Modele m = controleur.getModele();
@@ -80,7 +67,6 @@ public class View extends JFrame implements MouseInputListener{
         munition.setBackground(Color.gray);
         munition.setPreferredSize(new Dimension(this.getWidth() / 5, this.getHeight()));
 
-<<<<<<< HEAD
         partie = new JPanel(); // Partie du jeu, a droite de la fenetre
         partie.setLayout(new GridBagLayout());
         partie.setBackground(Color.darkGray);
@@ -138,71 +124,29 @@ public class View extends JFrame implements MouseInputListener{
         c.anchor = GridBagConstraints.NORTH;
         partie.add(canon, c);
         canon.setPreferredSize(new Dimension(250, 100));
-=======
-        // partie = new JPanel(){
-            partie = new viewPartieDroite(this);
-        //     @Override
-        //     public void paint(Graphics g) {
-        //         // TODO Auto-generated method stub
-        //         super.paint(g);
-        //         try{
-        //             Thread.sleep(20);
-        //         }catch(Exception e){}
-        //         repaint();
-        //     }
-        // }; // Partie du jeu, a droite de la fenetre
-        // partie.setLayout(null);
-        // partie.setBackground(Color.darkGray);
-        // partie.setPreferredSize(new Dimension(this.getWidth()*4/5,this.getHeight()));
-
-        // /*---- ELEMENTS DU JEU ----
-        //  * Canon
-        //  * Balle
-        //  * Obstacle(s)
-        // */
-        // canon = new JPanel();
-        // canon.setLayout(null);
-        // canon.setBounds((this.getWidth()*4/5)/2-25,0,50,100);
-        // canon.add(new JLabel("Canon"));
-        // partie.add(canon);
->>>>>>> matthew
 
         // /* Initialisation des variables de la balle */
         // this.xPos = this.getWidth()/2-25;
         // this.yPos = canon.getHeight()+20;
         // this.max_x = this.getWidth()-50;
         // this.max_y = this.getHeight()-50;
-        
+
         // balle = getBallPanel(m.getBalle());
         // partie.add(balle);
 
         // obstacles = new JPanel[1];
-        // JPanel obstacle1 = getObstaclesPanel(new Obstacle((this.getWidth()*4/5)/2-25,canon.getHeight()+100,75,75,0,true,100));
+        // JPanel obstacle1 = getObstaclesPanel(new
+        // Obstacle((this.getWidth()*4/5)/2-25,canon.getHeight()+100,75,75,0,true,100));
         // System.out.println(obstacle1.getWidth());
         // partie.add(obstacle1);
 
         /*---- FIN ELEMENTS DU JEU ----*/
-        
+
         c.anchor = GridBagConstraints.EAST;
         fond.add(munition);
         fond.add(partie, c);
 
-<<<<<<< HEAD
-        // -------Disposition du jeu-------
-
-        // -------Elements du jeu-------
-        /*
-         * balle = getBallPanel(m.getBalle());
-         * 
-         * //this.add(balle);
-         * this.add(new JLabel("test"), 0);
-         */
-        // -------Elements du jeu-------
-
         munition.add(leave, BorderLayout.SOUTH);
-=======
-        munition.add(leave,BorderLayout.SOUTH);
->>>>>>> matthew
         leave.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
@@ -210,7 +154,6 @@ public class View extends JFrame implements MouseInputListener{
         });
         this.add(fond);
         this.setVisible(true);
-<<<<<<< HEAD
         // this.pack();
 
         while (enJeu) {
@@ -223,51 +166,42 @@ public class View extends JFrame implements MouseInputListener{
                 e.printStackTrace();
             }
         }
-=======
         addMouseListener(this);
     }
 
     // private JPanel getBallPanel(Balle b) {
-    //     return new JPanel() {
-    //         @Override
-    //         public void paint(Graphics g) {
-    //             Graphics2D g2d = (Graphics2D) g;
-    //             g2d.setColor(Color.PINK);
-    //             g2d.fillOval((int)getxPos(), (int)getyPos(),(int) b.getWidth(), (int)b.getHeight());
+    // return new JPanel() {
+    // @Override
+    // public void paint(Graphics g) {
+    // Graphics2D g2d = (Graphics2D) g;
+    // g2d.setColor(Color.PINK);
+    // g2d.fillOval((int)getxPos(), (int)getyPos(),(int) b.getWidth(),
+    // (int)b.getHeight());
 
-    //             // setxPos(getxPos()+getXballSpeed());
-    //             // setyPos(getyPos()+getYballSpeed());
-    //             // System.out.println(getxPos() + " position x " + getyPos() + " position y");
+    // // setxPos(getxPos()+getXballSpeed());
+    // // setyPos(getyPos()+getYballSpeed());
+    // // System.out.println(getxPos() + " position x " + getyPos() + " position
+    // y");
 
-    //             try {
-    //                 Thread.sleep(30);
-    //             } catch (Exception e) {
-    //                 // TODO: handle exception
-    //             }
-    //             repaint();
-    //         }
-    //     };
+    // try {
+    // Thread.sleep(30);
+    // } catch (Exception e) {
+    // // TODO: handle exception
+    // }
+    // repaint();
+    // }
+    // };
     // }
     // private JPanel getObstaclesPanel(Obstacle obs){
-    //     return new JPanel() {
-    //         @Override
-    //         public void paint(Graphics g) {
-    //             Graphics2D g2d = (Graphics2D) g;
-    //             g2d.setColor(Color.YELLOW);
-    //             g2d.fillOval(0, 0, 75,75);
-    //         }
-    //     };
+    // return new JPanel() {
+    // @Override
+    // public void paint(Graphics g) {
+    // Graphics2D g2d = (Graphics2D) g;
+    // g2d.setColor(Color.YELLOW);
+    // g2d.fillOval(0, 0, 75,75);
     // }
-    public static void main(String[] args) {
-        View view = new View(new Controleur());
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        
->>>>>>> matthew
-
-    }
+    // };
+    // }
 
     @Override
     public void mousePressed(MouseEvent e) {
@@ -277,31 +211,31 @@ public class View extends JFrame implements MouseInputListener{
     @Override
     public void mouseReleased(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     public boolean isEnJeu() {
@@ -422,5 +356,11 @@ public class View extends JFrame implements MouseInputListener{
 
     public void setControleur(Controleur controleur) {
         this.controleur = controleur;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+
     }
 }
