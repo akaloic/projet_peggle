9dfbf102661f93078d76b179b176a74c4b21a602
JIRAUD Loïc <loicjiraud@MacBook-Pro-de-JIRAUD.local>
2023-03-09 00:36:29 +0100

    ajout de mes propres fonctions

--- src/controller/Controleur.java
+++ src/controller/Controleur.java
@@ -45,7 +45,7 @@ public class Controleur {
 
                 if (modele.balle != null) {
                     modele.balle.update(180 - getAngleTir(), t);
-                    if (modele.balle.y > view.getPartie().getHeight()) {
+                    if (modele.balle.y > View.partie.getHeight()) {
                         t = 0;
                     }
                     t += 0.3;
@@ -56,6 +56,31 @@ public class Controleur {
         });
         timer.start();
 
+    }
+
+    public Pegs collisionPeg() {
+
+        Balle b = modele.balle;
+        for (int i = 0; i < modele.niveau.list_peg.size(); i++) {
+            Pegs p = (Pegs) modele.niveau.list_peg.get(i);
+            double distance = Math.sqrt(Math.pow(b.getX() - p.getX(), 2) +
+                    Math.pow(b.getY() - p.getY(), 2));
+
+            if (distance <= b.rayon + p.rayon) {
+                System.out.println("x de balle : " + b.getX() + '\n' + "y de balle : " + b.getY() + '\n' + "x de peg : "
+                        + p.getX() + '\n' + "y de peg : " + p.getY() + '\n' + "distance : " + distance + '\n'
+                        + "rayon balle : " + b.rayon + '\n' + "rayon peg : " + p.rayon + '\n');
+                return p; // Collision détectée
+            }
+        }
+        return null;
+
+    }
+
+    public void tirer() {
+        this.modele.setBalle(null);
+        this.angleTir = this.view.getAngle();
+        this.modele.setBalle(new Balle(600d, 0d, 200d));
     }
 
     // ---------GETTER SETTER---------
@@ -74,15 +99,10 @@ public class Controleur {
     public void setModele(Modele modele) {
         this.modele = modele;
     }
-    // ---------GETTER SETTER---------
-
-    public void tirer() {
-        this.modele.setBalle(null);
-        this.angleTir = this.view.getAngle();
-        this.modele.setBalle(new Balle(600d, 0d, 200d));
-    }
 
     public double getAngleTir() {
         return this.angleTir;
     }
+    // ---------GETTER SETTER---------
+
 }
--- src/model/Pegs.java
+++ src/model/Pegs.java
@@ -2,25 +2,32 @@ package model;
 
 import view.View;
 import java.awt.*;
-public class Pegs extends Obstacle{
-    private double rayon=50;
-    public Pegs(){
+
+public class Pegs extends Obstacle {
+    public double rayon = 50;
+
+    public Pegs() {
         super(1);
     }
-    public Pegs(double x, double y, int v){
-        super(x, y, 50,50, false, v);
+
+    public Pegs(double x, double y, int v) {
+        super(x, y, 50, 50, false, v);
     }
-    public Pegs(double x, double y, int v,int rayon){
-        super(x, y, rayon,rayon, false, v);
-        this.rayon=rayon;
+
+    public Pegs(double x, double y, int v, int rayon) {
+        super(x, y, rayon, rayon, false, v);
+        this.rayon = rayon;
     }
-    public double getRayon(){return this.rayon;}
-    public double getDiametre(){return this.rayon*2;}
-    public void dessine(Graphics g){
-        int gx=(int)(this.x*View.getRatioX());
-        int gy=(int)(this.y*View.getRatioY());
-        int gw=(int)(this.rayon*View.getRatioX());
-        int gh=(int)(this.rayon*View.getRatioY());
-        g.fillOval(gx,gy,gw,gh);
+
+    public double getDiametre() {
+        return this.rayon * 2;
+    }
+
+    public void dessine(Graphics g) {
+        int gx = (int) (this.x * View.getRatioX());
+        int gy = (int) (this.y * View.getRatioY());
+        int gw = (int) (this.rayon * View.getRatioX());
+        int gh = (int) (this.rayon * View.getRatioY());
+        g.fillOval(gx, gy, gw, gh);
     }
 }
