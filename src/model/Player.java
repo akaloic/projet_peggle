44785e9d87fbc8668cddbc4c5bdc9f4ff7d05494
cesa <matthew.cesa@etu.u-paris.fr>
2023-05-12 09:03:38 +0200

    erreru de branche

---
+++ src/model/Player.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.security.cert.PolicyNode;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import controller.Sauvegarde;
+public class Player implements Serializable{
+    public int score;
+    public String pseudo;
+    public int progression = 5;
+    public int pointGagneParBalleEnJeu;
+    public int[]listeScore = new int[5];
+    public ArrayList<Integer> listeScoreEdit = new ArrayList<Integer>();
+    public ArrayList<ArrayList<Obstacle>> liste = new ArrayList<ArrayList<Obstacle>>();
+
+    public Player(String s) {
+        pseudo = s;
+        score = 0;
+        pointGagneParBalleEnJeu = 0;
+    }
+    public int calculScore(boolean detruit, int facteur, boolean balleEnJeu) {
+        pointGagneParBalleEnJeu += facteur;
+        if(balleEnJeu) {
+            if (detruit){
+                pointGagneParBalleEnJeu += 5;
+                if(pointGagneParBalleEnJeu >= 150 && pointGagneParBalleEnJeu< 300 ) {
+                    pointGagneParBalleEnJeu += 5*1.5;
+                }else if (pointGagneParBalleEnJeu >= 300 && pointGagneParBalleEnJeu< 500) {
+                    pointGagneParBalleEnJeu += 5*2;
+                }else if(pointGagneParBalleEnJeu >= 500 && pointGagneParBalleEnJeu< 700) {
+                    pointGagneParBalleEnJeu += 5*3;
+                }else if(pointGagneParBalleEnJeu > 700) {
+                    pointGagneParBalleEnJeu += 5*4;
+                }
+            }
+        }
+        score += pointGagneParBalleEnJeu;
+        if(pointGagneParBalleEnJeu > 5) {
+            pointGagneParBalleEnJeu -= 5;
+        }
+        return pointGagneParBalleEnJeu;
+        
+    }
+    public String getPseudo(){
+        return this.pseudo;
+    }
+    public void setPseudo(String s){
+        this.pseudo = s;
+    }
+    public void setScore(int i){
+        if(Sauvegarde.numNiveau == -1){
+            if( score > listeScore[i]){
+                listeScore[i] = score;
+            } 
+        }else{
+            if( score > listeScoreEdit.get(Sauvegarde.numNiveau)){
+                listeScoreEdit.set(Sauvegarde.numNiveau, score);
+            } 
+        }
+    }
+}
